0e56c30b4d0ae96c4c5ec39c699c36399814599b
Harshitha <harshitha@Harshithas-MacBook-Air.local>
2025-04-21 15:48:39 -0500

    Udf changed to create diveregence

--- run_udf.cuh
+++ run_udf.cuh
@@ -1,12 +1,14 @@
 #ifndef RUN_UDF_CUH
 #define RUN_UDF_CUH
 
+#include <cuda_runtime.h>
+
+// UDF that simulates input-dependent workload
 __device__ void run_udf(int input) {
     int sum = 0;
-    for (int i = 0; i < input; ++i) {
-        sum += i;
+    for (int i = 0; i < input * 1000; ++i) {
+        sum += i % 7; // Light computation inside loop
     }
 }
 
 #endif
-
